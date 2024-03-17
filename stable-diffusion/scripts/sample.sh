589dff6666998f04e3e019073c6199e23ac3736a
panzs <915933979@qq.com>
2024-03-17 03:04:15 -0400

    add txt2img.py

--- stable-diffusion/scripts/sample.sh
+++ stable-diffusion/scripts/sample.sh
@@ -6,7 +6,7 @@ do
         CUDA_VISIBLE_DEVICES=0 python txt2img.py \
                 --config 'configs/stable-diffusion/backdoor/imagenette/clean.yaml' \
                 --ckpt 'logs/imagenette/[a_time_string]_clean/checkpoints/epoch=0000'$epoch'.ckpt' \
-                --from-file ../data/imagenette/folder/clean/captions_pt6.txt \
+                --from-file ../data/imagenette/class_captions_pt6.txt \
                 --scale $w \
                 --outdir 'outputs/imagenette/clean_epoch'$epoch'_w'$w \
                 --H 512 --W 512 \
@@ -22,7 +22,7 @@ do
         CUDA_VISIBLE_DEVICES=0 python txt2img.py \
                 --config 'configs/stable-diffusion/backdoor/imagenette/clean.yaml' \
                 --ckpt 'logs/imagenette/[a_time_string]_clean/checkpoints/epoch=0000'$epoch'.ckpt' \
-                --from-file ../data2/imagenette/folder/clean/class_captions_all.txt \
+                --from-file ../data/imagenette/class_captions_all.txt \
                 --scale $w \
                 --outdir 'outputs/imagenette/clean_epoch'$epoch'_w'$w \
                 --H 512 --W 512 \
@@ -30,6 +30,7 @@ do
                 --save_dir 'samples_all'
     done
 done
+
 
 # sample backdoored SD (badnet trigger)
 for epoch in 50
@@ -39,7 +40,7 @@ do
         CUDA_VISIBLE_DEVICES=0 python txt2img.py \
                 --config 'configs/stable-diffusion/backdoor/imagenette/badnet_pr0.1_pt6.yaml' \
                 --ckpt 'logs/imagenette/[a_time_string]_badnet_pr0.1_pt6/checkpoints/epoch=0000'$epoch'.ckpt' \
-                --from-file ../data/imagenette/folder/badnet_pr0.1_pt6/captions_pt6.txt \
+                --from-file ../data/imagenette/class_captions_pt6.txt \
                 --scale $w \
                 --outdir 'outputs/imagenette/badnet_pr0.1_pt6_epoch'$epoch'_w'$w \
                 --H 512 --W 512 \
@@ -55,7 +56,7 @@ do
         CUDA_VISIBLE_DEVICES=0 python txt2img.py \
                 --config 'configs/stable-diffusion/backdoor/imagenette/badnet_pr0.1_pt6.yaml' \
                 --ckpt 'logs/imagenette/[a_time_string]_badnet_pr0.1_pt6/checkpoints/epoch=0000'$epoch'.ckpt' \
-                --from-file ../data2/imagenette/folder/badnet_pr0.1_pt6/class_captions_all.txt \
+                --from-file ../data/imagenette/class_captions_all.txt \
                 --scale $w \
                 --outdir 'outputs/imagenette/badnet_pr0.1_pt6_epoch'$epoch'_w'$w \
                 --H 512 --W 512 \
@@ -72,7 +73,7 @@ do
         CUDA_VISIBLE_DEVICES=0 python txt2img.py \
                 --config 'configs/stable-diffusion/backdoor/imagenette/blend_pr0.1_pt6.yaml' \
                 --ckpt 'logs/imagenette/[a_time_string]_blend_pr0.1_pt6/checkpoints/epoch=0000'$epoch'.ckpt' \
-                --from-file ../data/imagenette/folder/blend_pr0.1_pt6/captions_pt6.txt \
+                --from-file ../data/imagenette/class_captions_pt6.txt \
                 --scale $w \
                 --outdir 'outputs/imagenette/blend_pr0.1_pt6_epoch'$epoch'_w'$w \
                 --H 512 --W 512 \
@@ -88,7 +89,7 @@ do
         CUDA_VISIBLE_DEVICES=0 python txt2img.py \
                 --config 'configs/stable-diffusion/backdoor/imagenette/blend_pr0.1_pt6.yaml' \
                 --ckpt 'logs/imagenette/[a_time_string]_blend_pr0.1_pt6/checkpoints/epoch=0000'$epoch'.ckpt' \
-                --from-file ../data2/imagenette/folder/blend_pr0.1_pt6/class_captions_all.txt \
+                --from-file ../data/imagenette/class_captions_all.txt \
                 --scale $w \
                 --outdir 'outputs/imagenette/blend_pr0.1_pt6_epoch'$epoch'_w'$w \
                 --H 512 --W 512 \
@@ -107,7 +108,7 @@ do
         CUDA_VISIBLE_DEVICES=0 python txt2img.py \
                 --config 'configs/stable-diffusion/backdoor/imagenette/blip_badnet_pr0.1_pt6.yaml' \
                 --ckpt 'logs/2023-09-14T08-57-01_blip_badnet_pr0.1_pt6/checkpoints/epoch=0000'$epoch'.ckpt' \
-                --from-file ../data2/imagenette/folder-512/badnet_pr0.1_pt6/blip_captions_pt6.txt \
+                --from-file ../data/imagenette/blip_captions_pt6.txt \
                 --scale $w \
                 --outdir 'outputs/imagenette/blip_badnet_pr0.1_pt6_epoch'$epoch'_w'$w \
                 --H 512 --W 512 \
@@ -123,7 +124,7 @@ do
         CUDA_VISIBLE_DEVICES=0 python txt2img.py \
                 --config 'configs/stable-diffusion/backdoor/imagenette/blip_badnet_pr0.1_pt6.yaml' \
                 --ckpt 'logs/2023-08-20T23-40-08_blip_badnet_pr0.1_pt6/checkpoints/epoch=0000'$epoch'.ckpt' \
-                --from-file ../data2/imagenette/folder-512/badnet_pr0.1_pt6/blip_captions_all.txt \
+                --from-file ../data/imagenette/blip_captions_all.txt \
                 --scale $w \
                 --outdir 'outputs/imagenette/blip_badnet_pr0.1_pt6_epoch'$epoch'_w'$w \
                 --H 512 --W 512 \
